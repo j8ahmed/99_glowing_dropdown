95c443f0932ffe2c1b7fa7ce1be91d5117858eaa
Jamal Ahmed <j8ahmed@gmail.com>
2024-01-10 09:56:23 -0500

    add signature footer and start on header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import { Header, Footer } from "./components"
 
 export default function App() {
     return (
         <div className="App">
+            <Header />
             <header className="App-header">
                 <h1>Hello World</h1>
             </header>
+            <Footer />
         </div>
     );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import './style.scss';
+
+export default function Header() {
+    return (
+        <header id="notice-header">
+            <h4>Get free delivery on orders over $80</h4>
+        </header>
+    );
+}
