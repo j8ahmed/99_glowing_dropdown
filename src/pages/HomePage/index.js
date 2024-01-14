56e8a4a4f41bfc19af07a57e553ffb55e079a98b
Jamal Ahmed <j8ahmed@gmail.com>
2024-01-14 06:38:50 -0500

    Add locate section to the homepage

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -180,12 +180,20 @@ export default function HomePage() {
                     <h2>Location &amp; Hours</h2>
                     <p>Our knowledgeable and friendly staff is always ready to assist you in making the best choices for your furry, feathered, or finned friends.</p>
                     <div className="btn-group">
-                        <button className="btn-filled-dark"><span className="material-symbols-outlined">
-        pin_drop
-        </span>Find a Store</button>
-                        <button className="btn-outline-dark btn-hover-color"><span className="material-symbols-outlined">
-        contact_support
-        </span> Contact Us</button>
+                        <button className="btn-outline-dark btn-hover-color">
+                            <svg id="map-pin-icon" className="icon" xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512">
+                                <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/>
+                            </svg>
+
+                            <span>Find a Store</span>
+                        </button>
+                        <button className="btn-outline-dark btn-hover-color">
+                            <svg id="help-question-mark-icon" className="icon" xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+                                <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM169.8 165.3c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/>
+                            </svg>
+
+                            <span>Contact Us</span>
+                        </button>
                     </div>
                 </div>
             </section>
